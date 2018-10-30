3d793df9cef3f5279dcdfd360c1029ea34ca339a
Levi Wright <lwright@healthequity.com>
2018-10-29 18:39:39 -0600

    Second commit to make sure everything is working well in source control.

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicesService } from '../services.service';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
+  providers: [ServicesService],
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
