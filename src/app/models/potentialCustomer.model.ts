8db19ff5077e81761b6650a656ee312c28fdcd92
unknown <levi.spaace@protonmail.com>
2018-11-29 12:21:28 -0700

    added potentialCustomer class

---
+++ src/app/models/potentialCustomer.model.ts
@@ -0,0 +1,14 @@
+ 
+
+export class potentialCustomer {
+    constructor(
+        public name: string,
+        public emailAddress: string,
+        public phoneNumber: string,
+        public communicationPreference: string,
+        public primaryInterest: string,
+        public additionalComments: string
+    ){
+
+    }
+}
