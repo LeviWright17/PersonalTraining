22f1cc5d6966d5a662c146247845653c79e775a0
Levi Wright <lwright@healthequity.com>
2018-11-30 12:38:08 -0700

    done with major changes to the email form.

--- src/app/models/potentialCustomer.model.ts
+++ src/app/models/potentialCustomer.model.ts
@@ -1,11 +1,12 @@
  export class potentialCustomer {
-
     public name: string; 
     public emailAddress: string; 
     public phoneNumber: string; 
+    public traineeNumber: string;
+    public age: string;
+    public trainingDuration: string;
     public communicationPreference: string; 
     public primaryInterest: string; 
     public additionalComments: string; 
-
     constructor(){}
 }
