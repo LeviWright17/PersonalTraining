d87883ef092d34b84531a4505703787567782200
Levi Wright <lwright@healthequity.com>
2018-12-28 15:58:22 -0700

    Adding some more layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,29 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
-import { RouterModule } from '@angular/router'; 
-import { HttpClientModule } from '@angular/common/http'; 
-import { ReactiveFormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CustomErrorHandlerService } from './core/custom-error-handler-service.service';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     ContactComponent,
+    PricingComponent,
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      {path: 'about-us', component: AboutUsComponent },
-      {path: 'contact', component: ContactComponent },
-      {path: '', redirectTo: 'about-us', pathMatch: 'full'}
-    ], {useHash: true})
+      { path: 'about-us', component: AboutUsComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'pricing', component: PricingComponent },
+      { path: '', redirectTo: 'about-us', pathMatch: 'full' }
+    ], { useHash: true })
   ],
   providers: [
     { provide: ErrorHandler, useClass: CustomErrorHandlerService }
