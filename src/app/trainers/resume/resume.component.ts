01d3dbdba05618d29a77154538e625713a835580
Levi Wright <lwright@healthequity.com>
2019-01-25 13:38:16 -0700

    Able to set html into new component for the resume portion of the code.

--- src/app/trainers/resume/resume.component.ts
+++ src/app/trainers/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
+  @Input() showResumeFor: string;
+  private toggleLeviResume = 'Levi';
+  private toggleCeceResume = 'Cece';
+  public resumeContentToDisplay: string;
+  public leviResumeContent: string;
+  public ceceResumeContent: string;
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.resumeContentToDisplay = this.showResumeFor 
+    == this.toggleLeviResume ? this.leviResumeContent : this.ceceResumeContent;
+
+    this.resumeContentToDisplay = `<div>Hey there peeps</div>`
+  }; 
+
 
 }
