3894ae8d9fa20b07dfd6b327f6d0144ddb9aa420
Levi Wright <lwright@healthequity.com>
2019-01-29 15:23:48 -0700

    more styling

--- src/app/trainers/resume/resume.component.ts
+++ src/app/trainers/resume/resume.component.ts
@@ -11,7 +11,7 @@ export class ResumeComponent implements OnInit {
   private leviResume : string = 'levi'; 
   public resumeContentToDisplay: string;
   public leviResumeContent: string = `
-  <div class="jumbotron">
+  <div class="jumbotron resume-jumbo">
   <div class="container">
     <h2>Levi Wright</h2>
     <ul>
