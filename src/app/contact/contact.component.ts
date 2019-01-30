79843bf23953220ff67d12cf544128ac26bdef55
Levi Wright <lwright@healthequity.com>
2019-01-30 14:58:49 -0700

    Fixing and refactoring

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,7 @@ import { ContactService } from './contact.service';
 import { contactError } from '../models/contactError.model';
 import { matchEmailFields, matchPhoneFields } from './functions';
 import { renameMe } from '../models/renameMe.model';
+import { v } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-contact', //LLL figure out where this is referenced. 
@@ -78,8 +79,18 @@ export class ContactComponent implements OnInit {
 
   phoneValidationMessage: string;
   phoneValidationMessages = {
-    required: 'A valid phone number is required',
+    required: 'A valid phone number is required ',
+    minlength: 'Phone number must be 10 digits ', 
+    pattern: 'Phone number can only contain numbers'
   }
+
+  confirmPhoneValidationMessage : string; 
+  confirmPhoneValidationMessages = {
+    required: 'A valid phone number is required',  
+    minlength: 'Phone number must be 10 digits', 
+    pattern: ' Phone number can only contain numbers'
+  }
+
 
   emailError: string;
   phoneError: string;
@@ -89,6 +100,8 @@ export class ContactComponent implements OnInit {
   namecontrol: AbstractControl = new FormControl(); 
   emailControl: AbstractControl = new FormControl(); 
   confirmEmailControl: AbstractControl = new FormControl(); 
+  phoneControl: AbstractControl = new FormControl(); 
+  confirmPhoneControl: AbstractControl = new FormControl(); 
 
   constructor(private formBuilder: FormBuilder, private contactservice: ContactService,
     private titleService: Title) {
@@ -102,14 +115,16 @@ export class ContactComponent implements OnInit {
     this.namecontrol = this.contactForm.get('name'); 
     this.emailControl = this.contactForm.get('emailGroup.email'); 
     this.confirmEmailControl = this.contactForm.get('emailGroup.confirmEmail'); 
+    this.phoneControl = this.contactForm.get('phoneGroup.phone'); 
+    this.confirmPhoneControl = this.contactForm.get('phoneGroup.confirmPhone'); 
     this.setCommunicationPreference(this.contactForm.get('communicationPreference').value);
     this.watchNameControl();
     this.watchEmailGroup();
     this.watchPhoneGroup(); 
+    this.watchCommunicationPreferenceControlGroup(); 
   }
 
   public setCommunicationPreference(selectedCommunicationPreference: string): void {
-    // const confirmEmailControl = this.contactForm.get('emailGroup.confirmEmail');
     const phoneControl = this.contactForm.get('phoneGroup.phone');
     const confirmPhoneControl = this.contactForm.get('phoneGroup.confirmPhone');
 
@@ -121,7 +136,7 @@ export class ContactComponent implements OnInit {
       this.configureFieldsForPhoneCommunicationPreference(this.emailControl, this.confirmEmailControl,
         phoneControl, confirmPhoneControl);
     }
-    this.configureValueAndValidity(this.emailControl, phoneControl);
+    this.configureValueAndValidity();
   }
 
   public send() {
@@ -194,15 +209,35 @@ export class ContactComponent implements OnInit {
       this.phoneValidationMessage = Object.keys(c.errors).map(
         key => this.phoneValidationMessage += this.phoneValidationMessages[key]).join(' ');
     }
+
+    var newArray = []; 
+    console.log(this.phoneValidationMessage.split(' '), 'array split');
+    var result = this.phoneValidationMessage.split(' ').forEach(function(value){
+      if(!newArray.includes(value)){
+        newArray.push(value); 
+      }
+    })
+    console.log(newArray, 'THIS IS THE NEW ARRAY'); 
+  }
+
+
+  private setConfirmPhoneMessage(c: AbstractControl): void {
+    this.confirmPhoneValidationMessage = '';
+    if (this.fieldStateIsInvalid(c)) {
+      this.confirmPhoneValidationMessage = Object.keys(c.errors).map(
+        key => this.confirmPhoneValidationMessage += this.confirmPhoneValidationMessages[key]).join(' ');
+    }
   }
 
   private fieldStateIsInvalid(c: AbstractControl) {
     return (c.touched || c.dirty) && c.errors;
   }
 
-  private configureValueAndValidity(emailControl: AbstractControl, phoneControl: AbstractControl) {
-    emailControl.updateValueAndValidity();
-    phoneControl.updateValueAndValidity();
+  private configureValueAndValidity() {
+    this.emailControl.updateValueAndValidity();
+    this.confirmEmailControl.updateValueAndValidity(); 
+    this.phoneControl.updateValueAndValidity();
+    this.confirmPhoneControl.updateValueAndValidity(); 
   }
 
   private configureFieldsForPhoneCommunicationPreference(emailControl: AbstractControl, confirmEmailControl: AbstractControl, phoneControl: AbstractControl, confirmPhoneControl: AbstractControl) {
@@ -233,7 +268,7 @@ export class ContactComponent implements OnInit {
           validator: matchEmailFields
         }),
       phoneGroup: this.formBuilder.group({
-        phone: ['', [Validators.minLength(10), Validators.pattern("^[0-9]*$")]],
+        phone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
         confirmPhone: ['', [Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
       }, {
           validator: matchPhoneFields
@@ -261,7 +296,14 @@ export class ContactComponent implements OnInit {
   private watchPhoneGroup() {
     const phoneGroupControl = this.contactForm.get('phoneGroup'); 
     const phoneControl = phoneGroupControl.get('phone'); 
+    const confirmPhoneControl = phoneGroupControl.get('confirmPhone');
     phoneGroupControl.valueChanges.pipe(debounceTime(500)).subscribe(()=> this.setPhoneGroupMessage(phoneGroupControl)); 
     phoneControl.valueChanges.pipe(debounceTime(500)).subscribe(()=> this.setPhoneMessage(phoneControl)); 
+    confirmPhoneControl.valueChanges.pipe(debounceTime(500)).subscribe(()=> this.setConfirmPhoneMessage(confirmPhoneControl)); 
+  }
+
+  private watchCommunicationPreferenceControlGroup() {
+    const communicationPreferenceGroupControl = this.contactForm.get('communicationPreference'); 
+    communicationPreferenceGroupControl.valueChanges.subscribe(() => this.setCommunicationPreference(this.contactForm.get('communicationPreference').value))
   }
 }
